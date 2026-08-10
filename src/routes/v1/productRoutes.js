3a06e3b115c9c5a914cd97f6acef82ca6a2b074a
hassannoor230 <hassannoor2309@gmail.com>
2026-08-10 23:42:46 +0500

    feat: protect product/service write routes with admin auth

--- src/routes/v1/productRoutes.js
+++ src/routes/v1/productRoutes.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const router = express.Router()
 const productController = require('../../controllers/productController')
+const { protect, adminOnly } = require('../../middleware/auth')
 
 router.get('/', productController.listProducts)
 router.get('/slug/:slug', productController.getProduct)
 router.get('/:id', productController.getProductById)
+
+router.post('/', protect, adminOnly, productController.createProduct)
+router.put('/:id', protect, adminOnly, productController.updateProduct)
+router.delete('/:id', protect, adminOnly, productController.deleteProduct)
 
 module.exports = router
--- src/routes/v1/serviceRoutes.js
+++ src/routes/v1/serviceRoutes.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const router = express.Router()
 const serviceController = require('../../controllers/serviceController')
+const { protect, adminOnly } = require('../../middleware/auth')
 
 router.get('/', serviceController.listServices)
 router.get('/slug/:slug', serviceController.getService)
 router.get('/:id', serviceController.getServiceById)
+
+router.post('/', protect, adminOnly, serviceController.createService)
+router.put('/:id', protect, adminOnly, serviceController.updateService)
+router.delete('/:id', protect, adminOnly, serviceController.deleteService)
 
 module.exports = router
